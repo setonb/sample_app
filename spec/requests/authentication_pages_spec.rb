fc4b8ba27881d719586d60b41cd619567fae0245
Seton Brown <setonb@gmail.com>
2013-03-05 20:32:00 -0800

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -100,6 +100,20 @@ describe "Authentication" do
       end
     end
 
+    describe "in the Microposts controller" do
+
+      describe "submitting to the create action" do
+        before { post microposts_path }
+        it { response.should redirect_to(signin_path) }
+      end
+
+      describe "submitting to the destroy action" do
+        before { delete micropost_path(FactoryGirl.create(:micropost)) }
+        it { response.should redirect_to(signin_path) }
+      end
+    end
+
+
     describe "as wrong user" do
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
       before { sign_in user }
