78b87f0c4d950caf28cf4f597359086b7a55f0c5
Seton Brown <setonb@gmail.com>
2013-03-07 18:02:08 -0800

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -97,6 +97,16 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
       end
     end
 
@@ -112,6 +122,18 @@ describe "Authentication" do
         it { response.should redirect_to(signin_path) }
       end
     end
+    describe "in the Relationships controller" do
+
+      describe "submitting to the create action" do
+        before { post relationships_path }
+        it { response.should redirect_to(signin_path) }
+      end
+
+      describe "submitting to the destroy action" do
+        before { delete relationship_path(1) }
+        it { response.should redirect_to(signin_path) }
+      end    
+    end      
 
 
     describe "as wrong user" do
